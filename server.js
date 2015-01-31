c3c4b61d01d31030ad0a65868369cd36688988c3
Surendra <svsdesigns85@gmail.com>
2015-02-01 00:43:58 +0530

    request compression package

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('newrelic');
 console.log("starting");
 var port = process.env.PORT || 5000;
-process.env.PWD = process.cwd()
+process.env.PWD = process.cwd();
 
+var compression = require('compression');
 var express = require("express");
 
 var app = express();
+
+app.use(compression())
 
 app.use(express.static(process.env.PWD)); 
 app.get("/", function(request, response) {
